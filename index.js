b5ff29527da239bb164c44a1cbe206207d4d6f7c
umairali435 <53268680+umairali435@users.noreply.github.com>
2023-04-30 03:20:13 +0000

    hash maps

--- index.js
+++ index.js
@@ -5,14 +5,35 @@ class HashTable {
 
   _hash(key){
     let hash = 0;
-    for(let i=0; i < key.length; i++){
-      hash = (hash + key.charCodeAt(i)*i) %
-        this.data.length
+    for(let i = 0; i < key.length; i++){
+      hash = (hash + key.charCodeAt(i)*i) % this.data.length
     }
-    console.log(key.charCodeAt(0));
     return hash;
+  }
+
+  set(key,value){
+    let address = this._hash(key);
+    if(!this.data[address]){
+      this.data[address] = []
+    }
+    this.data[address].push([key,value])
+  }
+  get(key){
+    let address = this._hash(key);
+    let currentBucket = this.data[address]
+    if(currentBucket){
+      for(let i = 0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key){
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
   }
 }
 
 const myhashTable = new HashTable(50);
-myhashTable._hash("grapesaresour=")
+myhashTable._hash("grapes")
+myhashTable.set("grapess", 10000)
+myhashTable.set("apples", 50)
+console.log(myhashTable.get("grapess"))
